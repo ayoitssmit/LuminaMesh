f6aef46d7fd0df234aa968bb93e70c0f4f5d17a4
ayoitssmit <smitshah3005@gmail.com>
2026-03-30 12:47:40 +0530

    feat: turns server

--- src/lib/peerManager.ts
+++ src/lib/peerManager.ts
@@ -32,6 +32,7 @@ class NativePeer {
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
         { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun.cloudflare.com:3478" },
         {
           urls: "turn:openrelay.metered.ca:80",
           username: "openrelayproject",
@@ -44,6 +45,11 @@ class NativePeer {
         },
         {
           urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turns:openrelay.metered.ca:443?transport=tcp",
           username: "openrelayproject",
           credential: "openrelayproject"
         }
